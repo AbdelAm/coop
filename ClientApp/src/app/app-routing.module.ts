a5709f74af8ee351e532a2986a92c818acbc6e1a
reyamed <ahmed.aziz@ump.ac.ma>
2022-07-05 17:05:05 +0100

    forgot password interface
    added forgot password interface

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  {path: 'forgotpassword', component: ForgotPasswordComponent},
 
 
 ];
