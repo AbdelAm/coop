33844b4a5bc84472a873889e678eb191e58e6b31
Abdelaziz kissi <75265885+ITKISSI@users.noreply.github.com>
2022-07-05 17:19:14 +0100

    routing app-module

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
-
+import { RouterModule } from '@angular/router'
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
+<<<<<<< Updated upstream
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+=======
+import { LoginComponent } from './login/login.component';
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
@@ -17,10 +20,19 @@ import { ForgotPasswordComponent } from './forgot-password/forgot-password.compo
     HomeComponent,
     ForgotPasswordComponent,
   ],
+  
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
+<<<<<<< Updated upstream
     FormsModule,
+=======
+    BrowserModule, FormsModule, // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< And here
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'login', component: LoginComponent, pathMatch: 'full' },
+    ]),
+>>>>>>> Stashed changes
     AppRoutingModule
   ],
   providers: [],
