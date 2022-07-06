9391e9531b301b1b43cda9ca39b5aa98d8c6d82a
ABDELOUHAB AMMARI <abdelammari20@gmail.com>
2022-07-06 09:06:44 +0100

    add services and models forlders

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -7,32 +7,24 @@ import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
-<<<<<<< Updated upstream
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
-=======
 import { LoginComponent } from './login/login.component';
->>>>>>> Stashed changes
+import { AdministrateurModule } from './administrateur/administrateur.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
+    LoginComponent,
     ForgotPasswordComponent,
   ],
   
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
-<<<<<<< Updated upstream
     FormsModule,
-=======
-    BrowserModule, FormsModule, // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< And here
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'login', component: LoginComponent, pathMatch: 'full' },
-    ]),
->>>>>>> Stashed changes
+    AdministrateurModule,
     AppRoutingModule
   ],
   providers: [],
