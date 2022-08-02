a6ba0649247ce9dfe79355cea57210f2d9a79853
ABDELOUHAB AMMARI <abdelammari20@gmail.com>
2022-08-02 10:58:34 +0100

    add userId to request model

--- ClientApp/src/app/shared/models/request-model.ts
+++ ClientApp/src/app/shared/models/request-model.ts
@@ -3,6 +3,7 @@ import { StatusModel } from './status-model'
 export class RequestModel {
     id!: number;
     type!: string;
+    userId!: number;
     message!: string;
     status!: StatusModel;
 }
